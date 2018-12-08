7c3a6278f0300bfd45b92d93717b21cc94bc6286
Phuc Le <phucle>
2018-12-08 14:16:55 +0700

    change flash_screen

--- app/pages/home/home.component.ts
+++ app/pages/home/home.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit, ViewChild, Injectable, ChangeDetectorRef } from "@angular/core";
+import {Component, OnInit, ViewChild, Injectable, ChangeDetectorRef} from "@angular/core";
 import * as moment from 'moment';
-import { TranslateService } from "@ngx-translate/core";
-import { ShareDataService, SideDrawerService, OdooService, LocalStorageService, FirebaseService, MultiLanguageService,FacebookService } from "~/shared";
-import { Http } from "@angular/http";
-import { Page, Color } from "tns-core-modules/ui/page/page";
+import {TranslateService} from "@ngx-translate/core";
+import {
+    ShareDataService,
+    SideDrawerService,
+    OdooService,
+    LocalStorageService,
+    FirebaseService,
+    MultiLanguageService,
+    FacebookService
+} from "~/shared";
+import {Http} from "@angular/http";
+import {Page, Color} from "tns-core-modules/ui/page/page";
 import * as utils from "utils/utils";
 import * as application from "application";
 import * as frame from "ui/frame";
-import { config } from "~/config";
-import { RadSideDrawerComponent, SideDrawerType } from "nativescript-ui-sidedrawer/angular";
-import { RadSideDrawer } from 'nativescript-ui-sidedrawer';
+import {config} from "~/config";
+import {RadSideDrawerComponent, SideDrawerType} from "nativescript-ui-sidedrawer/angular";
+import {RadSideDrawer} from 'nativescript-ui-sidedrawer';
 import * as app from "tns-core-modules/application";
-import { ObservableArray } from "tns-core-modules/data/observable-array/observable-array";
-import { showLoadingIndicator, hideLoadingIndicator } from "~/utils";
+import {ObservableArray} from "tns-core-modules/data/observable-array/observable-array";
+import {showLoadingIndicator, hideLoadingIndicator} from "~/utils";
 // import { Message } from "nativescript-plugin-firebase";
-import { OdooClient } from "nativescript-odoo/odoo-api/odoo-client";
+import {OdooClient} from "nativescript-odoo/odoo-api/odoo-client";
 import * as fresco from "nativescript-fresco";
+
 @Component({
     selector: "ns-home",
     moduleId: module.id,
@@ -28,6 +37,7 @@ export class HomeComponent implements OnInit {
     public odooClient: OdooClient;
     private drawer: RadSideDrawer;
     public dataItems: ObservableArray<any>;
+
     constructor(
         public translateService: TranslateService,
         public shareDataService: ShareDataService,
@@ -41,7 +51,7 @@ export class HomeComponent implements OnInit {
         public http: Http,
         public page: Page
     ) {
-        this.dataItems = new ObservableArray([{ name: "Atalanta" }, { name: "Achilles" }]);
+        this.dataItems = new ObservableArray([{name: "Atalanta"}, {name: "Achilles"}]);
         console.log("multilaungue ", this.multiLanguageService.get("HOME_SCREEN.SUB_TITLE"));
         for (let i = 0; i < 40; i++) {
             this.dataItems.push({
@@ -52,21 +62,20 @@ export class HomeComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        if (application.ios) {
-            let applicationIos = application.ios.nativeApp;
-            application.ios.nativeApp.valueForKey("statusBarWindow").valueForKey("statusBar").backgroundColor = new Color(config.application.STATUS_COLOR).ios;
-        }
-        else {
-            application.android.startActivity.getWindow().setStatusBarColor(new Color(config.application.STATUS_COLOR).android);
-        }
+        // if (application.ios) {
+        //     let applicationIos = application.ios.nativeApp;
+        //     application.ios.nativeApp.valueForKey("statusBarWindow").valueForKey("statusBar").backgroundColor = new Color(config.application.STATUS_COLOR).ios;
+        // }
+        // else {
+        //     application.android.startActivity.getWindow().setStatusBarColor(new Color(config.application.STATUS_COLOR).android);
+        // }
         this.firebaseService.initFireBase({
             onMessageReceivedCallback: function (message: any) {
                 // alert(message.body);
                 if (message && message.body) {
                     console.log("success firebase : ", message)
                     // alert(message.body);
-                }
-                else {
+                } else {
                     console.log("undefined body callback firebase,", message);
                     // alert("undefined body callback firebase");
                 }
@@ -92,6 +101,7 @@ export class HomeComponent implements OnInit {
         // }, 10)
 
     }
+
     onTap(args) {
         // showLoadingIndicator();
         let self = this;
@@ -108,8 +118,7 @@ export class HomeComponent implements OnInit {
                         console.log("setSessionId  ", self.odooClient.getSessionId());
                         if (user.username) {
                             console.log("---login user: ", user);
-                        }
-                        else {
+                        } else {
                             console.log("undefined username");
                         }
                     })
@@ -138,13 +147,16 @@ export class HomeComponent implements OnInit {
                 console.log(err);
             })
     }
+
     public openDrawer(args) {
         console.log("openDreaw");
         this.sideDrawerService.openDrawer();
     }
+
     public closeDrawer(args) {
         this.sideDrawerService.closeDrawer();
     }
+
     refreshList(args) {
         console.log("args");
         args.object.refreshing = false;
